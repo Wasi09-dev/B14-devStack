73f815acf2c79407ba4fcf251704cf6f26bdc611
unknown <alwasia674@gmail.comgit config --global user.name>
2026-09-11 11:12:41 +0600

    changing button color

--- src/assets/components/Banner.tsx
+++ src/assets/components/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className='flex justify-between items-center'>
            <div className='max-w-2xl mx-auto py-8'>
             <h2 className='font-bold text-3xl decoration-[#0F0F2A]'>Build Your Ideal</h2>
-            <h2 className='font-bold text-3xl mb-3'>Development Stack</h2>
+            <h2 className='font-bold text-3xl bg-linear-to-r from-[#f07117] to-[rgb(67,69,228)] bg-clip-text text-transparent mb-3'>Development Stack</h2>
 <p>Explore frontend, backend, database, and tooling options,<br></br>
 compare them side by side, and put together the stack that fits your<br></br>
 next project.</p>
